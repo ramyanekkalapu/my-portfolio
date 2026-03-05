e052719a030e637569c71f216a0920dbe40f2601
ramyanekkalapu <78948510+ramyanekkalapu@users.noreply.github.com>
2026-03-05 17:19:24 -0600

    Update GalleryView.tsx

--- src/components/gallery/GalleryView.tsx
+++ src/components/gallery/GalleryView.tsx
@@ -1,23 +1,27 @@
-"use client";
+// "use client";
 
-import { Media, MasonryGrid } from "@once-ui-system/core";
-// import { gallery } from "@/resources";
+// import { Media, MasonryGrid } from "@once-ui-system/core";
+// // import { gallery } from "@/resources";
+
+// export default function GalleryView() {
+//   return (
+//     <MasonryGrid columns={2} s={{ columns: 1 }}>
+//       {gallery.images.map((image, index) => (
+//         <Media
+//           enlarge
+//           priority={index < 10}
+//           sizes="(max-width: 560px) 100vw, 50vw"
+//           key={index}
+//           radius="m"
+//           aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
+//           src={image.src}
+//           alt={image.alt}
+//         />
+//       ))}
+//     </MasonryGrid>
+//   );
+// }
 
 export default function GalleryView() {
-  return (
-    <MasonryGrid columns={2} s={{ columns: 1 }}>
-      {gallery.images.map((image, index) => (
-        <Media
-          enlarge
-          priority={index < 10}
-          sizes="(max-width: 560px) 100vw, 50vw"
-          key={index}
-          radius="m"
-          aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
-          src={image.src}
-          alt={image.alt}
-        />
-      ))}
-    </MasonryGrid>
-  );
+  return null;
 }
